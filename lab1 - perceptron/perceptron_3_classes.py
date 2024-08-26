16371375b5a7e673c1da12b1353e48de9b654373
Myxir294 <mik229944@gmail.com>
2024-08-26 11:59:29 +0200

    simple classification task (lab1) works as intended

--- lab1 - perceptron/perceptron_3_classes.py
+++ lab1 - perceptron/perceptron_3_classes.py
@@ -1,10 +1,10 @@
-#Program wykorzystuje model perceptronu do klasyfikacji danych ze zbioru iris z rozroznieniem na poszczegolne klasy
-#Wynikiem działania jest wypisanie ilości niepoprawnych klasyfikacji danych ze zbioru testujacego
+#Program uses Perceptron model to classify data from Iris dataset, dividing it to 3 separate classes.
+#In the result, the program provides its accuracy as the output
 
 import numpy as np
 import pandas as pd
 
-#Klasa modelujaca perceptron - wykorzystano propozycje klasy z repozytorium wskazanego przez prowadzącego zajęcia
+#Class providing Perceptron model - suggested by academic supervisor
 class Perceptron(object):
     
     def __init__(self, eta=0.01, epochs=50):
@@ -32,23 +32,22 @@ class Perceptron(object):
     def predict(self, X):
         return np.where(self.net_input(X) >= 0.0, 1, -1)
 
-#Wczytanie danych
+#Reading data
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
 #print(df.columns)
 
-#Zapisanie wyjsc odpowiadajacych wykorzystywanym klasom
-#Kazda tablica 150 elementow zawiera 1 w polach odpowiadajacych podanej nazwie i -1 w pozostalych
+#Saving outputs corresponding to each class
+#Each table of 150 elements contains 1 if class name is matching, -1 otherwise
 y2 = np.where(df[4] == 'Iris-setosa', 1, -1)
 y3 = np.where(df[4] == 'Iris-versicolor', 1, -1)
 y4 = np.where(df[4] == 'Iris-virginica', 1, -1)
 
-#Losowy wybor zestawow danych trenujacych stanowiacych okolo 80% calego zbioru danych
+#Random choosing of training data - about 80% of all the data, due to standard distribution of [0,1]
 x_train_id = np.random.rand(len(df)) < 0.8
 #print(x_train_id)
 #print(len(x_train_id))    
 
-#Wyselekcjonowanie pozadanych wartosci dla wybranych danych trenujacych
-#Oczekiwana liczebnosc kazdego z ponizszych to 120 
+#Providing values for training data - each vector is about 120 elements long
 y2_train = y2[x_train_id]
 y3_train = y3[x_train_id]
 y4_train = y4[x_train_id]
@@ -56,9 +55,8 @@ y4_train = y4[x_train_id]
 # print(y2_train)
 # print(len(y2_train))
 
-#Wyselekcjonowanie pozadanych wartosci dla wybranych danych testujacych
-#Negacje zastosowano w celu dopelnienia zbioru danych trenujacych
-#Oczekiwana liczebnosc kazdego z ponizszych to 30
+#Providing values for testing data
+#Each vector is about 30 elements long
 y2_test = y2[~x_train_id]
 y3_test = y3[~x_train_id]
 y4_test = y4[~x_train_id]
@@ -66,32 +64,32 @@ y4_test = y4[~x_train_id]
 # print(y2_test)
 # print(len(y2_test))
 
-#Wybor zestawu parametrow decydujacych o klasyfikacji obiektu do jednej z klas
+#Chossing parameters deciding the classification result
 X2_train = df.iloc[x_train_id, [0,1,2,3]].values #Dla danych trenujacych 
 X2_test = df.iloc[~x_train_id, [0,1,2,3]].values #Dla danych testujacych
 
-#Zdefiniowanie perceptronow - edycja wartosci epochs i eta bezsposrednio przeklada sie na jakosc nauki
+#Defining neurons - tweaking epochs and eta values changes learning results
 ppn2 = Perceptron(epochs=50, eta=0.001)
 ppn3 = Perceptron(epochs=50, eta=0.001)
 ppn4 = Perceptron(epochs=50, eta=0.001)
 
-#Trenowanie perceptronow
+#Training
 ppn2.train(X2_train, y2_train)
 ppn3.train(X2_train, y3_train)
 ppn4.train(X2_train, y4_train)
 
-#Wypisanie wynikow dla kazdego z perceptronow
+#Output results
 
 # print('Weights: %s' % ppn2.w_)
 # plot_decision_regions(X2_test, y2_test, clf=ppn2)
-# plt.title('Perceptron 1 - Klasyfikacja do Iris-setosa')
+# plt.title('Perceptron 1 - Iris-setosa classification')
 # plt.xlabel('sepal length [cm]')
 # plt.ylabel('petal length [cm]')
 # plt.show()
 
 print('Incorrect test data classifications for perceptron 1: %d' % ((y2_test != ppn2.predict(X2_test)).sum()))
 
-#Zapisanie tabeli okresjalacej prawdopodobienstwo nalezenia obiektu dla danej klasy
+#Saving tables of probability
 propability = ppn2.net_input(X2_test)
 
 #print(propability)
@@ -106,10 +104,10 @@ print('Incorrect test data classifications for perceptron 3: %d' % ((y4_test != 
 propability3 = ppn4.net_input(X2_test)
 #print(propability3)
 
-#Czesc programu odpowiadajaca za zestawienie wynikow
+#Output data processing 
 results = []
 
-#Petla dodaje do tablicy results najbardziej prawdopodobne wyniki
+#This loop adds most probable results to the 'results' list
 for prob1, prob2, prob3 in zip(propability.tolist(), propability2.tolist(), propability3.tolist()): 
   if max(prob1, prob2, prob3) == prob1:
     results.append('Iris-setosa')
@@ -118,13 +116,13 @@ for prob1, prob2, prob3 in zip(propability.tolist(), propability2.tolist(), prop
   elif max(prob1, prob2, prob3) == prob3:
     results.append('Iris-virginica')
 
-#Wizualizacja wynikow - porownanie wartosci uzyskanych z rzeczywistymi
+#Data presentation - comparison of output values and real ones
 #print(results)
 
-#"Wyciagniecie" z pliku prawdziwych nazw gatunkow 
+#"Extraction" of real species names from the file 
 y_real_value = df.iloc[~x_train_id, 4]
 
 #print(y_real_value)
 
-#Zsumowanie ilosci blednych klasyfikacji
-print('Total number of misclassifications (by probability) : %d na %d' % ((y_real_value != results).sum(), len(results)))
+#Counting of errors - how efficient we are
+print('Total number of misclassifications (by probability) : %d of %d' % ((y_real_value != results).sum(), len(results)))
